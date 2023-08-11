b827bd5fc34659a9055e5fac01ce789e8e35669c
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-11 20:08:42 +0200

    made post with userId possible

--- 02_lesson/src/features/posts/postsSlice.jsx
+++ 02_lesson/src/features/posts/postsSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
     { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things." },
@@ -10,11 +10,27 @@ const postsSlice = createSlice(
         name: 'posts',
         initialState,
         reducers: {
-
+            postAdded: {
+                reducer(state, action) {
+                    state.push(action.payload)
+                },
+                prepare(title, content, userId) {
+                    return {
+                        payload: {
+                            id: nanoid(),
+                            title,
+                            content,
+                            userId
+                        }
+                    }
+                }
+            }
         }
     }
 )
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
