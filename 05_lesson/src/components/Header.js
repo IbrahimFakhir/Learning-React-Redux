946ab99835aaba1b0c83d2665c94bbdee8225d9f
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-12 16:39:23 +0200

    implemented users interface

--- 05_lesson/src/components/Header.js
+++ 05_lesson/src/components/Header.js
@@ -7,7 +7,8 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="post">Post</Link></li>
+                    <li><Link to="post">Posts</Link></li>
+                    <li><Link to="user">Users</Link></li>
                 </ul>
             </nav>
         </header>
