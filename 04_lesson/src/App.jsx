6ce42ffd5fae3845ebc937f1b0471b264557ed99
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-12 14:30:45 +0200

    added browser router

---
+++ 04_lesson/src/App.jsx
@@ -0,0 +1,24 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+function App() {
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				
+				<Route index element={<PostsList />} />
+
+				<Route path="post">
+					<Route index element={<AddPostForm />} />
+					<Route path=":postId" element={<SinglePostPage />} /> 
+				</Route>
+
+			</Route>
+		</Routes>
+	);
+}
+
+export default App;
